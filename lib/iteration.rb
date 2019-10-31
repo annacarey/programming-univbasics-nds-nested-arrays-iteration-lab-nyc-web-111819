5cc56c46c2bf209a7b0969cdf4f5200906ef489a
declarative-blog-8005 <anna@annajcarey.com>
2019-10-31 20:57:20 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -43,7 +43,7 @@ def total_even_pairs(src)
   total = 0 
   index = 0 
   while index < src.count do 
-    if (src.index[0]%2 ==0 && src.index[1] %2 == 0)
+    if ((src.index[0] % 2 == 0) && (src.index[1] %2 == 0))
       total +=src.index[0]
       total +=src.index[1]
     end 
